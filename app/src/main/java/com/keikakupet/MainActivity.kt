8982ebf3fba859da58cccef791f695563f3bea9c
Muniba Khan <mkhan@rollins.edu>
2020-03-04 20:54:44 -0500

    added sample pet animation

--- app/src/main/java/com/keikakupet/MainActivity.kt
+++ app/src/main/java/com/keikakupet/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.keikakupet
 
 import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var petAnimation: AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // display pet animation
+        val petAnimation = findViewById<ImageView>(R.id.petImageView).apply {
+            setBackgroundResource(R.drawable.sample_pet_animation_list)
+            petAnimation = background as AnimationDrawable
+        }
 
         // launch ToDoListActivity
         val toDoListBtn = findViewById<Button>(R.id.toDoListBtn)
@@ -17,4 +28,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(startIntent)
         }
     }
+
+    override fun onStart() {
+        super.onStart()
+        petAnimation.start()
+    }
+
 }
