4ff76459ac194c30aec7dc0ee801f3c0a9fd7990
HeidiPo <kittenviolin@gmail.com>
2020-03-23 17:50:44 -0400

    applied adapter to the ToDoListActivity

--- app/src/main/java/com/keikakupet/ToDoListActivity.kt
+++ app/src/main/java/com/keikakupet/ToDoListActivity.kt
@@ -58,6 +58,7 @@ class ToDoListActivity : AppCompatActivity(), NewTaskDialogFragment.NewTaskDialo
         addTaskBtn.setOnClickListener {
             showNewTaskUI()
         }
+        //test
 
 
 
